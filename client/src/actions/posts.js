e720c8dff018a2c25cf00ca11b77d9a500a3894d
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-03 21:55:20 +0300

    Added decisions

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -31,6 +31,6 @@ export const updatePost = (id, post) => async (dispatch) => {
        dispatch({ type: 'UPDATE', payload: data });
 
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
     }
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid,CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
+
 import Post from './Post/Post';
 import useStyles from './styles';
 

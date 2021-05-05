1068769c2334a554ddd4918a34192093ac92bf37
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-05 20:43:25 +0300

    Added like functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -34,9 +34,9 @@ export const updatePost = async (req,res) => {
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
     
-    const updatePost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
 
-    res.json(updatePost);
+    res.json(updatedPost);
 }
 
 export const deletePost = async (req,res) => {
@@ -49,4 +49,15 @@ export const deletePost = async (req,res) => {
     console.log('DELETE!')
 
     res.json( { message: 'Post deleted successfully' } );
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost)
 }

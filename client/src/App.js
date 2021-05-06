b84526443c226e743b425800584feb44b12e3ca5
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-06 17:20:29 +0300

    Added functionality

--- client/src/App.js
+++ client/src/App.js
@@ -25,12 +25,14 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}></Grid>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
+                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId} />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={4}></Grid>
-                    <Form currentId={currentId} setCurrentId={setCurrentId} />
                 </Container>
             </Grow>
         </Container>

bdcd097cc1d8d767ac125ad649307bf0c5dc8883
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-11 20:58:24 +0300

    Remove same files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoos from 'mongoose';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config();
+//dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -15,11 +15,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-app.get('/', (req,res) => {
-    res.send('Hello to memories API')
-})
+// app.get('/', (req,res) => {
+//     res.send('Hello to memories API')
+// })
 
-//const CONNECTION_URL = 'mongodb+srv://JavaScript-Spa-Menu:mongodb548444@cluster0.onbxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://JavaScript-Spa-Menu:mongodb548444@cluster0.onbxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 

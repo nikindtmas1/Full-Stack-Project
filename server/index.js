4601b47862b907ef7f1156b2c995fa6c870dcfc3
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-27 23:00:02 +0300

    Added code server index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoos from 'mongoose';
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-//dotenv.config();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -19,11 +19,11 @@ app.use('/posts', postRoutes);
 //     res.send('Hello to memories API')
 // })
 
-const CONNECTION_URL = 'mongodb+srv://JavaScript-Spa-Menu:mongodb548444@cluster0.onbxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://JavaScript-Spa-Menu:mongodb548444@cluster0.onbxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
-mongoos.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoos.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
